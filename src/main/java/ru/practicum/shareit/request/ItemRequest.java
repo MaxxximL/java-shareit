b922d907d5ab5d266683870dc5764241eb26475d
MaxxximL <Maxiiiiiiiiim@yandex.ru>
2025-07-08 23:25:05 +0300

    15 - add-bookings(Database)

--- src/main/java/ru/practicum/shareit/request/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequest.java
@@ -1,7 +1,26 @@
 package ru.practicum.shareit.request;
 
-/**
- * TODO Sprint add-item-requests.
- */
+import jakarta.persistence.*;
+import lombok.*;
+import ru.practicum.shareit.user.User;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "item_requests")
 public class ItemRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "requester_id")
+    private User requester;
+
+    private LocalDateTime created;
+
 }
--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,12 +1,45 @@
 package ru.practicum.shareit.user;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
-/**
- * TODO Sprint add-controllers.
- */
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(path = "/users")
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public UserDto create(@RequestBody UserDto userDto) {
+
+        return UserMapper.toDto(userService.create(userDto));
+    }
+
+    @PatchMapping("/{userId}")
+    public UserDto update(@PathVariable Long userId,
+                          @RequestBody UserDto userDto) {
+        userDto.setId(userId);
+        return UserMapper.toDto(userService.update(userDto));
+    }
+
+    @GetMapping("/{userId}")
+    public UserDto getById(@PathVariable Long userId) {
+
+        return UserMapper.toDto(userService.getById(userId));
+    }
+
+    @GetMapping
+    public List<UserDto> getAll() {
+
+        return UserMapper.toDtoList(userService.getAll());
+    }
+
+    @DeleteMapping("/{userId}")
+    public void delete(@PathVariable Long userId) {
+
+        userService.delete(userId);
+    }
 }
